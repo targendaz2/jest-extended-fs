ef3e05d2f91d315a87c1e72f52071b62a4f1041b
David G Rosenberg <targendaz2@me.com>
2024-06-28 07:50:57 +0000

    Enabled github-actions Jest reporter

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,22 @@
 import { JestConfigWithTsJest } from 'ts-jest';
 
+type ReporterConfig = Exclude<
+  Required<JestConfigWithTsJest>['reporters'][number],
+  string
+>;
+
+const reporters: (string | ReporterConfig)[] =
+  process.env.CI === 'true'
+    ? [['github-actions', { silent: false }], 'summary']
+    : ['default'];
+
 const config: JestConfigWithTsJest = {
   moduleFileExtensions: ['js', 'ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  passWithNoTests: true,
   preset: 'ts-jest/presets/default-esm',
+  reporters,
   testEnvironment: 'node',
   testRegex: '.+\\.test\\.ts$',
 };
